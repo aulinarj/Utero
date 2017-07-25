7a9a983e4a34919c0fd19498e9bcb1ecb9212973
MaulanaFH <maulanafadia118@gmail.com>
2017-07-25 10:45:18 +0700

    login (not fixed)

---
+++ app/src/main/java/id/billboard/utero/uterobillboard/util/Server.java
@@ -0,0 +1,10 @@
+package id.billboard.utero.uterobillboard.util;
+
+/**
+ * Created by MaulanaFH on 7/25/2017.
+ */
+
+public class Server {
+    public static final String URL = "http://rizkiamrullah.000webhostapp.com/";
+}
+
